ff05cd11b1165758ce43d03aa5d95081d5356cd9
Henrique Neves <riquenlago36@gmail.com>
2022-10-05 11:29:21 -0300

    chore: create styles and list componet

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem';
 
-function App() {
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {
+      id: 1,
+      name: 'Comprar o Pão na Padaria',
+      done: false
+    },
+    {
+      id: 1,
+      name: 'Fazer compras no mercado',
+      done: true
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de tarefas</C.Header>
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item}></ListItem>
+        ))}
+      </C.Area>
+    </C.Container>
+  )
 }
 
 export default App;
---
+++ src/components/ListItem/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  background-color: #20212c;
+  padding: 10px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  align-items: center;
+
+  input {
+    width: 25px;
+    height: 25px;
+    margin: 0 5px 0 0;
+  }
+
+  label {
+    color: #ccc;
+  }
+`;
